a056fc8a4be6e6f6f277f0e63e0ff1ef86cdb47b
wahahoo <248516586@qq.com>
2023-04-27 09:48:41 +0800

    [feat]Elasticsearch基本操作练习

--- src/main/java/com/example/chart/controller/BlogController.java
+++ src/main/java/com/example/chart/controller/BlogController.java
@@ -6,23 +6,77 @@ import com.example.chart.entity.BlogModel;
 import com.example.chart.serviceI.BlogRepository;
 
 
+import com.example.chart.utils.Result;
+import com.example.chart.utils.ResultUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/blog")
 public class BlogController {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private BlogRepository blogRepository;
 
 
+
     @PostMapping("/add")
-    public Result add(@RequestBody BlogModel blogModel) {
+    public String add(@RequestBody BlogModel blogModel) {
+        logger.info(blogModel.toString());
         blogRepository.save(blogModel);
 
-        return Result.success();
+        return "success";
+    }
+    @GetMapping("/get/{id}")
+    public Result getById(@PathVariable String id) {
+        if (StringUtils.isEmpty(id))
+            return ResultUtils.fail("失败");
+        Optional<BlogModel> blogModelOptional = blogRepository.findById(id);
+        if (blogModelOptional.isPresent()) {
+            BlogModel blogModel = blogModelOptional.get();
+            return ResultUtils.success(blogModel);
+        }
+        return ResultUtils.fail("失败");
+    }
+    @GetMapping("/get")
+    public Result getAll() {
+        Iterable<BlogModel> iterable = blogRepository.findAll();
+        List<BlogModel> list = new ArrayList<>();
+        iterable.forEach(list::add);
+        return ResultUtils.success(list);
+    }
+    @PostMapping("/update")
+    public Result updateById(@RequestBody BlogModel blogModel) {
+        String id = blogModel.getId();
+        if (StringUtils.isEmpty(id))
+            return ResultUtils.fail("失败");
+        blogRepository.save(blogModel);
+        return ResultUtils.success("success");
+    }
+    @DeleteMapping("/delete/{id}")
+    public Result deleteById(@PathVariable String id) {
+        if (StringUtils.isEmpty(id))
+            return ResultUtils.fail("失败");
+        blogRepository.deleteById(id);
+        return ResultUtils.success("success");
+    }
+    @GetMapping("/rep/search/title")
+    public Result repSearchTitle(String keyword) {
+        if (StringUtils.isEmpty(keyword))
+            return ResultUtils.fail("失败");
+        return ResultUtils.success(blogRepository.findByTitleLike(keyword));
+    }
+    @GetMapping("/rep/search/custom")
+    public Result repSearchCustom(String keyword) {
+        if (StringUtils.isEmpty(keyword))
+            return ResultUtils.fail("失败");
+        return ResultUtils.success(blogRepository.findByTitleCustom(keyword));
     }
 }
--- src/main/java/com/example/chart/serviceI/BlogRepository.java
+++ src/main/java/com/example/chart/serviceI/BlogRepository.java
@@ -1,8 +1,13 @@
 package com.example.chart.serviceI;
 
 import com.example.chart.entity.BlogModel;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface BlogRepository extends ElasticsearchRepository<BlogModel, String> {
+import java.util.List;
 
+public interface BlogRepository extends ElasticsearchRepository<BlogModel, String> {
+    List<BlogModel> findByTitleLike(String keyword);
+    @Query("{\"match_phrase\":{\"title\":\"?0\"}}")
+    List<BlogModel> findByTitleCustom(String keyword);
 }
